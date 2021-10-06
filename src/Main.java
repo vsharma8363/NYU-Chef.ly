abfd84486766424d8edf08dc42dc21c5519ea355
dleguisamo <david.leguisamo@nyu.edu>
2021-10-06 19:53:36 -0400

    Introduce GUI to application. Basic GUI with scene switching implemented

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+//JavaFX Imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+
 import java.util.TreeMap;
 import Recipes.Recipe;
 import Recipes.RecipeManager;
 
-public class Main {
+public class Main extends Application {
+
+	//GUI start method
+	@Override
+    public void start(Stage primaryStage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setResizable(false);
+            String css = this.getClass().getResource("application.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		RecipeManager recipeManager = new RecipeManager("recipes");
 		for(Recipe recipe:recipeManager.getRecipeList()) {
 			System.out.println("\nName:\n" + recipe.getName());
@@ -18,6 +42,8 @@ public class Main {
 				System.out.println(recipeSteps.get(i+1));
 			}
 		}
+
+		launch(args);
 	}
 
 }
---
+++ src/SceneController.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToHomeScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToBrowseScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("BrowseScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToCreateScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("CreateScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToDiscoverScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("DiscoverScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    
+}
