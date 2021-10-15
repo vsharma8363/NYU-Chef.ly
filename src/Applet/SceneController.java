6b4ca870113c970d74a88f552df8a13bb53d65a2
Rebecca <rebecca_517@hotmail.com>
2021-10-15 16:56:34 -0400

    create gui wip

--- src/Applet/SceneController.java
+++ src/Applet/SceneController.java
@@ -1,24 +1,39 @@
 package Applet;
+
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
-public class SceneController {
+public class SceneController implements Initializable {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+    // Create Tab
+    @FXML
+    private ChoiceBox<String> difficultyChoiceBox;
+
+    private String[] difficultyChoices = { "easy", "medium", "hard" };
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // difficultyChoiceBox.getItems().addAll(difficultyChoices);
+    }
 
     public void switchToHomeScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -26,7 +41,7 @@ public class SceneController {
 
     public void switchToBrowseScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("BrowseScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -34,18 +49,17 @@ public class SceneController {
 
     public void switchToCreateScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("CreateScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        // Button btn = new Button();
-        // btn.setText("Recipe 1");
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        // difficultyChoiceBox.getItems().addAll(difficultyChoices);
     }
 
     public void switchToDiscoverScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("DiscoverScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
